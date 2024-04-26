f7e933e77121ac905feaa6059006d876862d6e01
chrisrca <goalie.cyclone@gmail.com>
2024-04-26 18:49:53 -0400

    added custom node shapes

--- src/dataGraph.tsx
+++ src/dataGraph.tsx
@@ -59,21 +59,57 @@ const GraphMesh: React.FC<{ position: Vector3 }> = ({ position }) => {
             });
     }, []);
 
+    const getMeshByNodeType = (nodeType: string) => {
+        switch (nodeType) {
+            case "HALL":
+                return { mesh: <boxGeometry args={[1, 1, 1]} />, color: "#0a2ce1" };
+            case "CONF":
+                return { mesh: <boxGeometry args={[2, 1, 1]} />, color: "#25982a" };
+            case "DEPT":
+                return { mesh: <sphereGeometry args={[0.5, 32, 32]} />, color: "#e04545" };
+            case "ELEV":
+                return { mesh: <cylinderGeometry args={[0.5, 0.5, 1, 32]} />, color: "#da8f08" };
+            case "EXIT":
+                return { mesh: <cylinderGeometry args={[0.5, 0.5, 1, 6]} />, color: "#ddde1f" };
+            case "INFO":
+                return { mesh: <coneGeometry args={[0.5, 1, 3]} />, color: "#1fe18a" };
+            case "LABS":
+                return { mesh: <octahedronGeometry args={[0.5, 0]} />, color: "#9608da" };
+            case "REST":
+                return { mesh: <boxGeometry args={[1, 1, 1]} />, color: "#5cdbda" }; // Parallelogram shape not directly available, using box for example
+            case "BATH":
+                return { mesh: <boxGeometry args={[1, 1, 1]} />, color: "#ce24d4" }; // Trapezoid shape not directly available, using box for example
+            case "RETL":
+                return { mesh: <cylinderGeometry args={[0.5, 0.5, 1, 8]} />, color: "#0b6009" };
+            case "STAI":
+                return { mesh: <cylinderGeometry args={[0.5, 0.5, 1, 5]} />, color: "#82a7f6" };
+            case "SERV":
+                return { mesh: <cylinderGeometry args={[0.5, 0.5, 1, 7]} />, color: "#67c537" };
+            default:
+                return { mesh: <boxGeometry args={[0.5, 0.5, 0.5]} />, color: "gray" };
+        }
+    };
+
     return (
         <>
-            {nodes.map((node, index) => (
-                <mesh
-                    key={index}
-                    position={[((position.x - 50) + (node.coords.x / 50)), 0, ((position.z - 34) + (node.coords.y / 50))]}
-                    rotation={[0, 0, 0]}
-                    scale={hoveredNode === node.id ? [0.5, 0.5, 0.5] : [0.25, 0.25, 0.25]}
-                    onPointerOver={() => setHoveredNode(node.id)}
-                    onPointerOut={() => setHoveredNode(null)}
-                >
-                    <boxGeometry args={[0.75, 0.75, 0.75]} />
-                    <meshBasicMaterial color="royalblue" />
-                </mesh>
-            ))}
+            {nodes.map((node, index) => {
+                const { mesh, color } = getMeshByNodeType(node.type);
+                return (
+                    <mesh
+                        key={index}
+                        position={[
+                            ((position.x - 50) + (node.coords.x / 50)), 0, ((position.z - 34) + (node.coords.y / 50))
+                        ]}
+                        rotation={[0, 0, 0]}
+                        scale={hoveredNode === node.id ? [0.5, 0.5, 0.5] : [0.25, 0.25, 0.25]}
+                        onPointerOver={() => setHoveredNode(node.id)}
+                        onPointerOut={() => setHoveredNode(null)}
+                    >
+                        {mesh}
+                        <meshBasicMaterial color={color} />
+                    </mesh>
+                );
+            })}
             {edges.map((edge, index) => {
                 const startNode = nodes.find(node => node.id === edge.start);
                 const endNode = nodes.find(node => node.id === edge.end);
