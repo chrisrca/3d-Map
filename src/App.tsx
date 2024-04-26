a2cd655198336278ba338cd4d6f6d097c11be033
chrisrca <goalie.cyclone@gmail.com>
2024-04-26 01:57:29 -0400

    added 3d element to map

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import React, { useRef, useState } from 'react';
-import { Canvas, useLoader, useThree } from '@react-three/fiber';
-import { TextureLoader, Vector3, Euler } from 'three';
-import { OrbitControls } from '@react-three/drei';
+import React, { useEffect, useRef, useState } from 'react';
+import { Canvas, useLoader } from '@react-three/fiber';
+import { TextureLoader, Vector3, Euler, Mesh, MeshStandardMaterial, Color } from 'three';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 import { THREE } from 'aframe';
 import img from "./img.png";
 import "./App.css"
 
 const MapCanvas: React.FC = () => {
+    const { nodes } = useGLTF('/floormap.glb');
+    const customMesh = nodes.CustomObject as Mesh;
     const meshRef = useRef<THREE.Mesh>(null);
     const texture = useLoader(TextureLoader, img);
     const aspectRatio = 5000 / 3400;
@@ -14,7 +16,11 @@ const MapCanvas: React.FC = () => {
     const height = width / aspectRatio;
     const [isDragging, setIsDragging] = useState(false);
     const [position, setPosition] = useState(new Vector3(0, 0, 0));
-    const [rotation, setRotation] = useState(new Euler(-Math.PI / 2, 0, 0));  // Face up by default
+    const [rotation] = useState(new Euler(-Math.PI / 2, 0, 0));  // Face up by default
+
+    const customMaterial = new MeshStandardMaterial({
+        color: new Color(0xe9d3b7) // Using numeric format
+    });
 
     const handlePointerDown = (event: { stopPropagation: () => void; }) => {
         setIsDragging(true);
@@ -48,12 +54,13 @@ const MapCanvas: React.FC = () => {
                 <meshBasicMaterial map={texture} />
             </mesh>
             <mesh
-                position={position}
-                rotation={rotation}
-            >
-                <boxGeometry args={[1, 1, 1]} />
-                <meshBasicMaterial color="royalblue" />
-            </mesh>
+                castShadow
+                receiveShadow
+                geometry={customMesh.geometry}
+                material={customMaterial}
+                position={[(position.x - 49.99), 0, (position.z + 34)]}
+                scale={0.4}
+            />
         </>
         
     );
@@ -63,7 +70,7 @@ const App: React.FC = () => {
     return (
         <div className='map-container'>
             <Canvas camera={{ fov: 75, position: new Vector3(0, 5, 5) }} style={{ height: '100vh', width: '100vw' }}>
-                <ambientLight intensity={0.5} />
+                <ambientLight intensity={3} />
                 <pointLight position={new Vector3(10, 10, 10)} />
                 <OrbitControls enableRotate={false} enableZoom={false} enablePan={false} />
                 <MapCanvas />
